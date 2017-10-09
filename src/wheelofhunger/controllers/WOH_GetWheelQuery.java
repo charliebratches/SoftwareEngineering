6b5987ce83e7874d780de1d4dbac780e66086de2
aperdew <perdewaaron@gmail.com>
2017-10-09 17:12:24 -0500

    So many fixes
    -Changed Wheel.html to a jsp file.
    -Moved it outside of the Wheel folder
    -Using sessions to pass data from one
      servlet to another
    -Created new servlet called "GetWheelQuery" thats just for the wheel ajax call
    -WheelQuery no longer responses to wheel's ajax call.

---
+++ src/wheelofhunger/controllers/WOH_GetWheelQuery.java
@@ -0,0 +1,51 @@
+package wheelofhunger.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import wheelofhunger.util.JSONconverter;
+
+/**
+ * Servlet implementation class WOH_GetWheelQuery
+ */
+@WebServlet("/GetWheelQuery")
+public class WOH_GetWheelQuery extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WOH_GetWheelQuery() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(true);
+		response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        
+        out.println(JSONconverter.convert(session.getAttribute("restaurantList")));
+        out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
